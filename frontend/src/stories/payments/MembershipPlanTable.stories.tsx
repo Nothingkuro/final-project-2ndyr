b5a6f83c76b082fc7652a2df43fdbccad9be3641
Vince <vinceamiel.latabe-24@cpu.edu.ph>
2026-04-10 12:08:45 +0800

    test: fix minor import issue in membership plan table stories

--- frontend/src/stories/payments/MembershipPlanTable.stories.tsx
+++ frontend/src/stories/payments/MembershipPlanTable.stories.tsx
@@ -5,6 +5,7 @@ import {
   storyPaymentPlans,
   storyPlansWithLongDescription,
   storyPlansWithMissingDescription,
+  mockManyMembershipPlans,
 } from '../mocks/mockMembershipPlans';
 
 const meta = {
