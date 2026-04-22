cb422d0100bc186b235052f35a29f90c3a5b2cbe
Vince <vinceamiel.latabe-24@cpu.edu.ph>
2026-04-22 13:28:26 +0800

    Revert "feat: add dashboard charts section component"
    This reverts commit c2c94ae93bdcb55940578d898898d6b260bc546f.

--- frontend/src/components/reports/index.ts
+++ frontend/src/components/reports/index.ts
@@ -2,4 +2,3 @@ export { default as DailyRevenueSummaryCard } from './DailyRevenueSummaryCard';
 export { default as MonthlyRevenueReportCard } from './MonthlyRevenueReportCard';
 export { default as MembershipExpiryAlertList } from './MembershipExpiryAlertList';
 export { default as LowInventoryAlertList } from './LowInventoryAlertList';
-export { default as DashboardChartsSection } from './DashboardChartsSection';
